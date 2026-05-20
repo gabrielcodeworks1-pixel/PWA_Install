57e59230fe8793bf7bad8862ab767a88d548d9e9
gabrielcodeworks1-pixel <gabriel.code.works1@gmail.com>
2026-05-20 20:33:55 -0300

    V2.3

--- main.js
+++ main.js
@@ -17,13 +17,15 @@ let aAlan = new Aluno("Alan", 41, "Ciencia da Computação")
 
 // -- Service Worker --
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./meuserviceworker.js')
-        .then(function(registration) {
-            console.log('Service Worker registered: ', registration);
-        })
-        .catch(function(error) {
-            console.log('Service Worker registration failed: ', error);
-        });
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./meuserviceworker.js')
+            .then(function(registration) {
+                console.log('Service Worker registered: ', registration);
+            })
+            .catch(function(error) {
+                console.log('Service Worker registration failed: ', error);
+            });
+    });
 } else {
     console.log('Service Workers are not supported in this browser.');
 }
--- meuserviceworker.js
+++ meuserviceworker.js
@@ -1,3 +1,25 @@
 self.addEventListener('install', function(event) {
     console.log('Service Worker instalado');
+    event.waitUntil(
+        caches.open('pwa-cache-v1').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/main.js',
+                '/manifest.json',
+                '/icon-192.png',
+                '/icon-512.png',
+                '/meuserviceworker.js',
+                '/style.css'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
